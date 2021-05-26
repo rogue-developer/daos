b1ce6e7353e47a00d71307a145bcba877a31dee9
Brian J. Murrell <brian.murrell@intel.com>
2021-05-26 14:49:32 -0400

    Update node prep
    Skip-PR-comments: true
    PR-repos-el7:    mpich@PR-47 ior@PR-1:11 dtcmp@PR-4 lwgrp@PR-4 libcircle@PR-4 hdf5-vol-daos@PR-18 hdf5@PR-24 mpifileutils-pkg@PR-14:3 daos@PR-5695:14
    PR-repos-el8:    mpich@PR-47 ior@PR-1:11 dtcmp@PR-4 lwgrp@PR-4 libcircle@PR-4 hdf5-vol-daos@PR-18 hdf5@PR-24 mpifileutils-pkg@PR-14:3 daos@PR-5695:14
    PR-repos-leap15: mpich@PR-47 ior@PR-1:11 dtcmp@PR-4 lwgrp@PR-4 libcircle@PR-4 hdf5-vol-daos@PR-18 hdf5@PR-24 mpifileutils-pkg@PR-14:3 daos@PR-5695:14
    RPM-test-version: 1.3.0-16.6407.g5dfc13ad
    Quick-Functional: true
    Test-tag: pr,ior pr,mdtest pr,datamover daily_regression,ior daily_regression,mdtest daily_regression,datamover test_core_files
    Skip-func-test-vm: true
    Skip-func-test-leap15: true
    Skip-func-test-el8: false
    Skip-func-hw-test: false
    Func-hw-test-distro: centos8
    Allow-unstable-test: true
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- ci/functional/test_main_prep_node.sh
+++ ci/functional/test_main_prep_node.sh
@@ -3,23 +3,24 @@
 set -eux
 
 for i in 0 1; do
-    if [ -e /sys/class/net/ib$i ]; then
-        if ! ifconfig ib$i | grep "inet "; then
+    iface="ib$i"
+    if [ -e /sys/class/net/"$iface" ]; then
+        if ! ifconfig "$iface" | grep "inet "; then
           {
-            echo "Found interface ib$i down after reboot on $HOSTNAME"
+            echo "Found interface $iface down after reboot on $HOSTNAME"
             systemctl status || true
             systemctl --failed || true
             journalctl -n 500 || true
-            ifconfig ib"$i" || true
-            cat /sys/class/net/ib$i/mode || true
-            ifup ib"$i" || true
+            ifconfig "$iface" || true
+            cat /sys/class/net/"$iface"/mode || true
+            ifup "$iface" || true
             ifconfig -a || true
-            cat /etc/sysconfig/network-scripts/ifcfg-ib"$i"
-            if ! ifconfig ib"$i" | grep "inet "; then
-                echo "Failed to bring up interface"
+            cat /etc/sysconfig/network-scripts/ifcfg-"$iface"
+            if ! ifconfig "$iface" | grep "inet "; then
+                echo "Failed to bring up interface $iface.  Please file a CORCI ticket."
                 exit 1
             fi
-          } # | mail -s "Interface found down after reboot" "$OPERATIONS_EMAIL"
+          } | mail -s "Interface $iface found down after reboot" "$OPERATIONS_EMAIL"
         fi
     fi
 done
